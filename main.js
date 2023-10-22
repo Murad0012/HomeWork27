9364b31351798038e286659a7a1b96697b2de9e3
Predator <murad.mammedzade11@gmail.com>
2023-10-22 03:20:37 -0700

    Save

---
+++ main.js
@@ -0,0 +1,51 @@
+//1
+
+// var student = {
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12 
+// };
+// var keyNames = Object.keys(student);
+// console.log(keyNames);
+
+//2
+
+// var student = {
+//     name : "David Rayy",
+//     sclass : "VI",
+//     rollno : 12 
+// };
+// delete student.rollno;
+// var keyNames = Object.keys(student);
+// console.log(keyNames);
+
+//3
+
+// const objWithSum = {
+//     a: 10,
+//     b: 10
+// };
+  
+// const newObjWithSum = {sum: objWithSum.a + objWithSum.b };
+  
+// console.log(newObjWithSum);
+  
+//4
+
+// const objWithRes = {
+//     n: 13,
+//     info: "n is smaller than 20"
+// };
+  
+// var updatedInfo;
+  
+// if (objWithRes.n > 10) {
+//     updatedInfo = "n is bigger than 10";
+// } else {
+//     updatedInfo = objWithRes.info;
+// }
+  
+// const newObjWithRes = {info: updatedInfo};
+  
+// console.log(newObjWithRes);
+  
